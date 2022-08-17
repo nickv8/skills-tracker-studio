a0c868c90cd43fd6969135a334d90e29a3179e54
Nick Villone <n.villone8@gmail.com>
2022-08-16 23:45:58 -0400

    created first method with html

--- main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -1,6 +1,25 @@
 package org.launchcode.skillstracker.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class SkillsController {
 
-
+@GetMapping()
+    public String languages(){
+    return "<html>" +
+            "<body>" +
+            "<h1>Skills Tracker</h1>" +
+            "<h2>Languages</h2>" +
+            "<ol>" +
+            "<li>JavaScript</li>" +
+            "<li>Python</li>" +
+            "<li>Java</li>" +
+            "</ol>" +
+            "</body>" +
+            "</html>";
+}
 }
